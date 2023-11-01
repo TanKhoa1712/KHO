7762ba663786cfa2339bcdd88d47fb7f3674d39c
TanKhoa1712 <147955987+TanKhoa1712@users.noreply.github.com>
2023-11-01 07:22:16 +0700

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    char a[15];
+    char freetime[] = "ko biet gi het";
+printf ("dot hai %s \n", freetime)/
+    printf ( "nhap ten Sv \n");
+    gets (a);
+    printf("Ten Sv da luu nhu sau %s \n" ,a);
+    }
